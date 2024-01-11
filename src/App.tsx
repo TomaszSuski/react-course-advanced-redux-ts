0af662d1059d7e944cc924f680774ccd8f41d3b9
Tomasz Suski <tomasz.suski@orba.co>
2024-01-11 16:11:39 +0100

    added redux to CartButton and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
+import React from "react";
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
+import { CartInterface } from "./store/cart";
 
 function App() {
+  const cartVisible = useSelector((state: CartInterface) => state.cart.cartVisible);
   return (
     <Layout>
-      <Cart />
+      {cartVisible && <Cart />}
       <Products />
     </Layout>
   );
--- src/components/Cart/CartButton.tsx
+++ src/components/Cart/CartButton.tsx
@@ -1,10 +1,20 @@
-import classes from './CartButton.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./CartButton.module.css";
+import { CartInterface, cartActions } from "../../store/cart";
 
 const CartButton = () => {
+  const dispatch = useDispatch();
+  const cartItemsCount = useSelector((state: CartInterface) =>
+    state.cart.items.reduce((acc, item) => acc + item.quantity, 0)
+  );
+
+  const toggleCartHandler = () => {
+    dispatch(cartActions.toggleCart());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartItemsCount}</span>
     </button>
   );
 };
